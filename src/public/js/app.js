4a1281eb3e87c047dceae41007c3fb9082d9cd90
Jae Yeon Lee <138093128+jaeyeon2854@users.noreply.github.com>
2024-06-13 16:51:00 +0900

    서로 message 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageUl = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open",()=>{
@@ -12,6 +14,16 @@ socket.addEventListener("close",()=>{
     console.log("Disconnected from the Server ❌");
 })
 
-setTimeout(()=> {
-    socket.send("hello from the browser!");
-},10000)
+// setTimeout(()=> {
+//     socket.send("hello from the browser!");
+// },10000)
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    // console.log(input.value);
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit",handleSubmit)
--- src/server.js
+++ src/server.js
@@ -17,11 +17,15 @@ const handleListen = () => console.log(`Linstening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+const sockets = [];
+
 wss.on("connection",(socket) =>{
+    sockets.push(socket)
     console.log("Connected to Browser ✅")
     socket.on("close",()=> console.log("Disconnected from the Browser ❌"))
     socket.on("message", (message) =>{
-        console.log(message)
+        const messageString = message.toString('utf8');
+        sockets.forEach(aSocket => aSocket.send(messageString))
     })
     socket.send("hello!")
 })
